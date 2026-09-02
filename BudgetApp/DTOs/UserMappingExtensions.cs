0785db44973d150cd084d3af12df2a7a94994cf2
martinamotlochova <137939946+martinamotlochova@users.noreply.github.com>
2026-09-02 15:33:46 +0200

    DbContext, MIgrations, Mapping DTOs

---
+++ BudgetApp/DTOs/UserMappingExtensions.cs
@@ -0,0 +1,16 @@
+﻿using BudgetApp.Models;
+namespace BudgetApp.DTOs
+{
+    public static class UserMappingExtensions
+    {
+        public static UserResponse ToDto(this User user)
+        {
+            return new UserResponse
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Name = user.Name
+            };
+        }
+    }
+}
---
+++ BudgetApp/DTOs/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace BudgetApp.DTOs
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
