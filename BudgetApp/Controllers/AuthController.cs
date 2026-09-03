616a2a9c3624409916d393ea56b96843001aa4a9
martinamotlochova <137939946+martinamotlochova@users.noreply.github.com>
2026-09-03 14:55:26 +0200

    Register Endpoint

---
+++ BudgetApp/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BudgetApp.Data;
+using BudgetApp.Models;
+using BudgetApp.DTOs;
+
+
+
+namespace BudgetApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController :ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            if (await _context.Users.AnyAsync(u => u.Email == request.Email))
+            {
+                return BadRequest("Email is already in use.");
+            }
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            var user = request.ToEntity(passwordHash);
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(user.ToDto());
+        }
+
+    }
+}
