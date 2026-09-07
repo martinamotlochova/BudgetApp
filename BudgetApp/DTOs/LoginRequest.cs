7b3157519d2c9725626d1aff54a6bf4d312ce199
martinamotlochova <137939946+martinamotlochova@users.noreply.github.com>
2026-09-07 10:36:24 +0200

    LoginRequestDTO

---
+++ BudgetApp/DTOs/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BudgetApp.DTOs
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
--- BudgetApp/Program.cs
+++ BudgetApp/Program.cs
@@ -3,10 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
